977a41cbb46f7f9b59da379294a505dfcda72b5a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-10 00:17:33 +0800

    Make script to manage form

---
+++ Assets/Scripts/FormManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FormManager : MonoBehaviour
+{
+    [SerializeField] private InputField inputTitle,
+        inputBody;
+
+    private string titleText, bodyText;
+    
+    public void SaveNote()
+    {
+        if(!string.IsNullOrWhiteSpace(inputTitle.text) && 
+           !string.IsNullOrWhiteSpace(inputBody.text))
+        {
+            titleText = inputTitle.text;
+            bodyText = inputBody.text;
+            Debug.Log("Title: " + titleText);
+            Debug.Log("Body: " + bodyText);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPrefsConstant.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsConstant
+{
+    [Tooltip("ID of each notes")] 
+    public static string Id = "Id";
+    
+    [Tooltip("Title of a note. \nHow to access: NoteTitle + ID")]
+    public static string NoteTitle = "NoteTitle";
+    
+    [Tooltip("Body of a note. \nHow to access: NoteBody + ID")]
+    public static string NoteBody = "NoteBody";
+}
